c4eaec4e1b53764b9d398ae8a1697a5de994e7fd
Vedsubramanian <52442368+Vedsubramanian@users.noreply.github.com>
2019-07-03 16:59:11 +0530

    Update RunCukesTest.java

--- src/main/java/runner/RunCukesTest.java
+++ src/main/java/runner/RunCukesTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features={"src/test/resources/addtocart.feature"},
+@CucumberOptions(features={"src/test/resources/addtocart.feature","src/test/resources/search.feature","src/test/resources/signin.feature","src/test/resources/signup.feature"},
 glue="Stepdefinition",
 plugin="json:target/cucumber.json")
 public class RunCukesTest {
